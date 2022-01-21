3b8bbfa69be24e9fe043205287434fea54d78386
Jose Perez <josecarlosperez.jlp@gmail.com>
2022-01-21 11:21:25 -0300

    barra de navegacion

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import './navbar.css'
+
+const navbar = () => {
+    return (     <nav>
+            <ul>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Nosotros</li>
+                <li>Contacto</li>
+            </ul>
+        <hr></hr>    
+        </nav>
+        
+    );
+}
+
+export default navbar;
