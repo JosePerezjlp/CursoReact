f90a712671f9b5a5f36749d44e97d62579e92c83
Jose Perez <josecarlosperez.jlp@gmail.com>
2022-01-25 15:28:39 -0300

    cardwidget

---
+++ src/components/cardwidget.js
@@ -0,0 +1,10 @@
+import { Icon } from "@mui/material"
+
+
+const CardWidget = () =>{
+    return (
+       <button><Icon color="secondary">shoppingcart</Icon></button>
+    )
+}
+
+export default CardWidget;
