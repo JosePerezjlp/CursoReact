bc583a80b85d22240b3f2006542fc52c86eb77bb
Jose Perez <josecarlosperez.jlp@gmail.com>
2022-01-28 11:57:47 -0300

    agregando cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 
 import './App.css';
 import Navbar from './components/navbar';
+import ItemListContainer from './components/itemlistcontainer';
+import motorola from './components/motorola-g8.jpg'
+import intel from './components/notebook-intel.jpg'
+import genius from './components/parlantes-genius.jpg'
 
 function App() {
+
   return (
     <>
-     <Navbar></Navbar>
+     <Navbar/>
+     <div className='cardProducto'>
+     <ItemListContainer
+      producto='Celular'
+      img={motorola}
+      marca='Motorola g8'
+      precio='$29000'
+      stock='10'
+     />
+      <ItemListContainer
+      producto='Notebook'
+      img={intel}
+      marca='Intel '
+      precio='$11000'
+      stock='20'
+     />
+      <ItemListContainer
+      producto='Parlante'
+      img={genius}
+      marca='Genius'
+      precio='$8000'
+      stock='5'
+     />
+     </div>
      </>
 );
 }
